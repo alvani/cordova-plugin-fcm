6e87c6e422e3e5b503840e623e03956c49c3c298
Alvani Wiwoho <alpha2_yui@yahoo.com>
2018-09-06 11:27:20 +0700

    Remove cordova define

--- www/FCMPlugin.js
+++ www/FCMPlugin.js
@@ -1,4 +1,3 @@
-cordova.define("cordova-plugin-fcm.FCMPlugin", function(require, exports, module) {
 var exec = require('cordova/exec');
 
 function FCMPlugin() { 
@@ -59,4 +58,3 @@ exec(function(result){ console.log("FCMPlugin Ready OK") }, function(result){ co
 
 var fcmPlugin = new FCMPlugin();
 module.exports = fcmPlugin;
-});
